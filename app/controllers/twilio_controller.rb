cc7859fe6b83ddb341fe2e0a2d681878c30a13e2
Tibet Sprague <tsprague@Tibetbook.local>
2012-04-29 12:16:17 -0700

    test

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -104,7 +104,8 @@ class TwilioController < ApplicationController
     def sms_response(text)
       logger.error "sms" + text + ENV.inspect
       Twilio.connect(ENV['TWILIO_KEY'], ENV["TWILIO_SECRET"])
-      Twilio::Sms.message(ENV["TWILIO_NUMBER"], @from_phone, text)
+      r = Twilio::Sms.message(ENV["TWILIO_NUMBER"], @from_phone, text)
+      logger.error r.inspect
       render :nothing => true
     end
 end
